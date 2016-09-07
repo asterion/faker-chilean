09d4d4ff85648a8164fbbf6d8a1de54c4d2d1861
Marcos Matamala <asterion.alfa@gmail.com>
2016-09-07 08:45:37 -0300

    Provider names products

--- example.php
+++ example.php
@@ -5,9 +5,14 @@ require_once 'vendor/autoload.php';
 $faker = Faker\Factory::create();
 $faker->addProvider(new Tidor\FakerChilean\Provider\Run($faker));
 $faker->addProvider(new Tidor\FakerChilean\Provider\Category($faker));
+$faker->addProvider(new Tidor\FakerChilean\Provider\Product($faker));
 
 echo $faker->run . PHP_EOL;
 echo $faker->unique()->rut . PHP_EOL;
 
 echo $faker->category . PHP_EOL;
 echo $faker->unique()->category . PHP_EOL;
+
+
+echo $faker->productName . PHP_EOL;
+echo $faker->unique()->productName . PHP_EOL;
