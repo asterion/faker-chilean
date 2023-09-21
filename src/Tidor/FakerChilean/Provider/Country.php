4bbbbceebd534e4228e3b239d80ef4f0c3f8f0b3
Marcos Matamala <mmatamala1982@gmail.com>
2023-09-20 21:12:30 -0300

    [add] Country Name and Country List

---
+++ src/Tidor/FakerChilean/Provider/Country.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tidor\FakerChilean\Provider;
+
+use Faker\Provider\Base as ProviderBase;
+
+/**
+ * Provider Country Names
+ *
+ * @link https://www.registrocivil.cl/Manuales/Validacion_de_Run.pdf Validación
+ */
+class Country extends ProviderBase
+{
+    public function country()
+    {
+        $data = $this->countryList();
+
+        $codes = array_keys($data);
+        $code = $codes[rand(0, count($codes) - 1)];
+
+        return $data[$code];
+    }
+
+    public function countryList()
+    {
+        $filename = realpath(__DIR__ . '/../../../../vendor/umpirsky/country-list/data/es_CL') . '/country.php';
+
+        return require($filename);
+    }
+}
